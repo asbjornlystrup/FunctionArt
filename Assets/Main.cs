1bf88d232c6ae3c8b6251b011f1cb12dbe4bbbeb
Asbjørn Fintland Lystrup <asbjorn.lystrup@gmail.com>
2020-02-13 01:12:39 +0100

    input image and polar coordinates

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour {
     struct Operator {
         public uint type;
         public int inputCount;
+        public string formatString;
+    }
+
+    struct InputValue {
+        public uint type;
         public string formatString;
     }
 
@@ -15,7 +21,8 @@ public class Main : MonoBehaviour {
         public Node parent = null;
         public List<Node> children = new List<Node>();
         public uint data;
-        public Operator _operator; 
+        public Operator _operator;
+        public InputValue inputValue;
     }
 
     class Generation {
@@ -31,6 +38,7 @@ public class Main : MonoBehaviour {
     public Vector3Int threadGroups;
     public Vector3Int textureSize = new Vector3Int(256, 256, 1);
     public Texture2D cpuTexture;
+    public Texture2D inputTexture;
     private bool showCpuVersion = false;
     private bool animate = true;
 
@@ -48,11 +56,122 @@ public class Main : MonoBehaviour {
     const uint AND = 4;
     const uint SUBTRACT = 5;
     const uint CONSTANT = 1000;
-    const uint INPUT = 1001;
+    const uint INPUT_X = 1001;
+    const uint INPUT_Y = 1002;
+    const uint INPUT_CHANNEL = 1003;
+    const uint INPUT_TIME = 1004;
+    const uint INPUT_ANGLE = 1005;
+    const uint INPUT_LENGTH = 1006;
+    const uint INPUT_TEXTURE_RGB = 1007;
 
     public int time = 0;
 
+    private List<Operator> operators = new List<Operator>();
+    private List<InputValue> inputValues = new List<InputValue>();
+
+    Node CreateAst(int instructionCount) {
+        List<Node> openNodes = new List<Node>();
+
+        Node root = null;
+        
+        // Create operator nodes
+        for (int i = 0; i < instructionCount; i++) {
+            Operator _operator = operators[Random.Range(0, operators.Count)];
+            
+            Node newNode = new Node();
+            newNode.type = NodeType.Operator;
+            newNode._operator = _operator;
+            for (int j = 0; j < _operator.inputCount; j++) {
+                newNode.children.Add(null);
+            }
+
+            if (openNodes.Count == 0) {
+                root = newNode;
+            } else {
+                Node parent = openNodes[Random.Range(0, openNodes.Count)];
+
+                List<int> openChildIndices = new List<int>();
+                for (int j = 0; j < parent.children.Count; j++) {
+                    if (parent.children[j] == null) {
+                        openChildIndices.Add(j);
+                    }
+                }
+                int childIndex = openChildIndices[Random.Range(0, openChildIndices.Count)];
+                parent.children[childIndex] = newNode;
+                newNode.parent = parent;
+
+                if (openChildIndices.Count == 1) {
+                    openNodes.Remove(parent);
+                }
+            }
+
+            openNodes.Add(newNode);
+        }
+
+        // Create leaf nodes
+        foreach (Node parent in openNodes) {
+            for (int i = 0; i < parent.children.Count; i++) {
+                if (parent.children[i] == null) {
+                    NodeType childNodeType = Random.value < 0.5 ? NodeType.Constant : NodeType.Input;
+                    if (childNodeType == NodeType.Constant) {
+                        Node newNode = new Node();
+                        newNode.type = NodeType.Constant;
+                        newNode.data = (uint)((long)Random.Range(int.MinValue, int.MaxValue) - (long)int.MinValue);
+                        parent.children[i] = newNode;
+                        newNode.parent = parent;
+                    } else if (childNodeType == NodeType.Input) {
+                        Node newNode = new Node();
+                        newNode.type = NodeType.Input;
+                        InputValue inputValue = inputValues[Random.Range(0, inputValues.Count)];
+                        newNode.inputValue = inputValue;
+                        newNode.data = inputValue.type;
+                        parent.children[i] = newNode;
+                        newNode.parent = parent;
+                    }
+                }
+            }
+        }
+
+        return root;
+    }
+
     void Awake() {
+        {
+            // Cool circular mixed with xy, lots of variation and curves
+            {
+                operators.Add(new Operator {type = XOR, inputCount = 2, formatString = "({0} ^ {1})"});
+                operators.Add(new Operator {type = ADD, inputCount = 2, formatString = "({0} + {1})"});
+                operators.Add(new Operator {type = MULTIPLY, inputCount = 2, formatString = "({0} * {1})"});
+                operators.Add(new Operator {type = OR, inputCount = 2, formatString = "({0} | {1})"});
+                operators.Add(new Operator {type = AND, inputCount = 2, formatString = "({0} & {1})"});
+                operators.Add(new Operator {type = SUBTRACT, inputCount = 2, formatString = "({0} - {1})"});
+
+                inputValues.Add(new InputValue {type = INPUT_X, formatString = "x"});
+                inputValues.Add(new InputValue {type = INPUT_Y, formatString = "y"});
+                inputValues.Add(new InputValue {type = INPUT_CHANNEL, formatString = "c"});
+                inputValues.Add(new InputValue {type = INPUT_TIME, formatString = "t"});
+                inputValues.Add(new InputValue {type = INPUT_ANGLE, formatString = "ang"});
+                inputValues.Add(new InputValue {type = INPUT_LENGTH, formatString = "len"});
+            }
+
+            operators.Add(new Operator {type = XOR, inputCount = 2, formatString = "({0} ^ {1})"});
+            operators.Add(new Operator {type = ADD, inputCount = 2, formatString = "({0} + {1})"});
+            operators.Add(new Operator {type = MULTIPLY, inputCount = 2, formatString = "({0} * {1})"});
+            operators.Add(new Operator {type = OR, inputCount = 2, formatString = "({0} | {1})"});
+            operators.Add(new Operator {type = AND, inputCount = 2, formatString = "({0} & {1})"});
+            operators.Add(new Operator {type = SUBTRACT, inputCount = 2, formatString = "({0} - {1})"});
+
+            inputValues.Add(new InputValue {type = INPUT_X, formatString = "x"});
+            inputValues.Add(new InputValue {type = INPUT_Y, formatString = "y"});
+            inputValues.Add(new InputValue {type = INPUT_CHANNEL, formatString = "c"});
+            inputValues.Add(new InputValue {type = INPUT_CHANNEL, formatString = "c"});
+            inputValues.Add(new InputValue {type = INPUT_CHANNEL, formatString = "c"});
+            inputValues.Add(new InputValue {type = INPUT_TIME, formatString = "t"});
+            inputValues.Add(new InputValue {type = INPUT_ANGLE, formatString = "ang"});
+            inputValues.Add(new InputValue {type = INPUT_LENGTH, formatString = "len"});
+            inputValues.Add(new InputValue {type = INPUT_TEXTURE_RGB, formatString = "rgb"});
+        }
+
         cellBackgroundTexture = new Texture2D(1, 1);
         cellBackgroundTexture.SetPixel(0, 0, new Color(0, 0, 0, 1));
         cellBackgroundTexture.Apply();
@@ -77,7 +196,7 @@ public class Main : MonoBehaviour {
     void Update() {
         bool updateTextures = false;
 
-        if (Input.GetMouseButtonDown(1)) {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
             updateTextures = true;
             time = 0;
             for (int i = 0; i < generations.Length; i++) {
@@ -172,13 +291,13 @@ public class Main : MonoBehaviour {
                 if (generation.programBuffer != null) {
                     int kernelIndex = cs.FindKernel("Main");
                     cs.SetTexture(kernelIndex, "Texture", generation.texture);
+                    cs.SetTexture(kernelIndex, "InputTexture", inputTexture);
                     cs.SetBuffer(kernelIndex, "ProgramBuffer", generation.programBuffer);
                     cs.SetInt("ProgramBufferElementCount", generation.programBuffer.count);
                     cs.SetInt("Time", time);
                     cs.Dispatch(kernelIndex, threadGroups.x, threadGroups.y, threadGroups.z);
                 }
             }
-            Debug.Log("Time: " + time);
         } 
     }
 
@@ -212,10 +331,9 @@ public class Main : MonoBehaviour {
             programList.Add(node.data);
             return memoryIndex;
         } else if (node.type == NodeType.Input) {
-            programList.Add(INPUT); // "Set input" operator id
+            programList.Add(node.inputValue.type); // "Set input" operator id
             uint memoryIndex = nextOpenMemoryIndex++;
             programList.Add(memoryIndex);
-            programList.Add(node.data);
             return memoryIndex;
         } else {
             Debug.LogError("Unknown node type");
@@ -229,6 +347,7 @@ public class Main : MonoBehaviour {
         int outline = 2;
         int totalWidth = gap + (textureSize.x + gap) * generationsX;
         int totalHeight = gap + (textureSize.y + gap) * generationsY;
+        string s = "";
         for (int y = 0; y < generationsY; y++) {
             for (int x = 0; x < generationsX; x++) {
                 if (i < generations.Length) {
@@ -238,86 +357,39 @@ public class Main : MonoBehaviour {
                     GUI.skin.box.normal.background = cellBackgroundTexture;
                     GUI.Box(new Rect(x1 - outline, y1 - outline, texture.width + outline * 2, texture.height + outline * 2), GUIContent.none);
                     GUI.DrawTexture(new Rect(x1, y1, texture.width, texture.height), texture);
+                    s += i + ": " + generations[i].text + "\n";
                 }
                 i++;
             }
         }
+
+        float textAreaWidth = Screen.width / 2 - totalWidth / 2 - gap - gap;
+        float textAreaHeight = Screen.height - gap * 2;
+
+        GUI.skin.textArea.focused.background = null;
+        GUI.skin.textArea.hover.background = null;
+
+        GUI.skin.textArea.alignment = TextAnchor.UpperLeft;
+        GUI.skin.textArea.normal.background = null;
+        GUI.skin.textArea.normal.textColor = new Color(0f, 0f, 0f, 0.4f);
+        GUI.TextArea(new Rect(gap, gap, textAreaWidth, textAreaHeight), s);
         
+        s = "Operators:\n";
+        foreach (Operator _operator in operators) {
+            s += _operator.formatString + "\n";
+        }
+        s += "\nInputs:\n";
+        foreach (InputValue inputValue in inputValues) {
+            s += inputValue.formatString + "\n";
+        }
+        GUI.skin.textArea.normal.background = null;
+        GUI.skin.textArea.normal.textColor = new Color(0f, 0f, 0f, 0.4f);
+        GUI.skin.textArea.alignment = TextAnchor.UpperRight;
+        GUI.TextArea(new Rect(Screen.width - 1 - gap - textAreaWidth, gap, textAreaWidth, textAreaHeight), s);
+
         if (showCpuVersion) {
             GUI.DrawTexture(new Rect(264f, 0f, 256f, 256f), cpuTexture);
         }
-    }
-    
-    Node CreateAst(int instructionCount) {
-        List<Node> openNodes = new List<Node>();
-        List<Operator> operators = new List<Operator>();
-        operators.Add(new Operator {type = 0, inputCount = 2, formatString = "({0} ^ {1})"});
-        operators.Add(new Operator {type = 1, inputCount = 2, formatString = "({0} + {1})"});
-        operators.Add(new Operator {type = 2, inputCount = 2, formatString = "({0} * {1})"});
-        operators.Add(new Operator {type = 3, inputCount = 2, formatString = "({0} | {1})"});
-        operators.Add(new Operator {type = 4, inputCount = 2, formatString = "({0} & {1})"});
-        operators.Add(new Operator {type = 5, inputCount = 2, formatString = "({0} - {1})"});
-        int inputCount = 4;
-
-        Node root = null;
-        
-        // Create operator nodes
-        for (int i = 0; i < instructionCount; i++) {
-            Operator _operator = operators[Random.Range(0, operators.Count)];
-            
-            Node newNode = new Node();
-            newNode.type = NodeType.Operator;
-            newNode._operator = _operator;
-            for (int j = 0; j < _operator.inputCount; j++) {
-                newNode.children.Add(null);
-            }
-
-            if (openNodes.Count == 0) {
-                root = newNode;
-            } else {
-                Node parent = openNodes[Random.Range(0, openNodes.Count)];
-
-                List<int> openChildIndices = new List<int>();
-                for (int j = 0; j < parent.children.Count; j++) {
-                    if (parent.children[j] == null) {
-                        openChildIndices.Add(j);
-                    }
-                }
-                int childIndex = openChildIndices[Random.Range(0, openChildIndices.Count)];
-                parent.children[childIndex] = newNode;
-                newNode.parent = parent;
-
-                if (openChildIndices.Count == 1) {
-                    openNodes.Remove(parent);
-                }
-            }
-
-            openNodes.Add(newNode);
-        }
-
-        // Create leaf nodes
-        foreach (Node parent in openNodes) {
-            for (int i = 0; i < parent.children.Count; i++) {
-                if (parent.children[i] == null) {
-                    NodeType childNodeType = Random.value < 0.5 ? NodeType.Constant : NodeType.Input;
-                    if (childNodeType == NodeType.Constant) {
-                        Node newNode = new Node();
-                        newNode.type = NodeType.Constant;
-                        newNode.data = (uint)((long)Random.Range(int.MinValue, int.MaxValue) - (long)int.MinValue);
-                        parent.children[i] = newNode;
-                        newNode.parent = parent;
-                    } else if (childNodeType == NodeType.Input) {
-                        Node newNode = new Node();
-                        newNode.type = NodeType.Input;
-                        newNode.data = (uint)Random.Range(0, inputCount);
-                        parent.children[i] = newNode;
-                        newNode.parent = parent;
-                    }
-                }
-            }
-        }
-
-        return root;
     }
 
     string ComposeAstString(Node node) {
@@ -333,7 +405,7 @@ public class Main : MonoBehaviour {
         } else if (node.type == NodeType.Constant) {
             return node.data.ToString();
         } else if (node.type == NodeType.Input) {
-            return "In" + node.data;
+            return node.inputValue.formatString;
         } else {
             return "N/A";
         }
